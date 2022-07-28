8830c271620a9f3a8260f2aa4f0e6b2cd187a37c
Miguel-Sanchez241001 <msanchez24@autonoma.edu.pe>
2022-07-28 14:07:32 -0500

    variables y tipos de datos

---
+++ concatenacion.dart
@@ -0,0 +1,7 @@
+void main() {
+  String nombre = "Luis";
+  print("el nombre del usuario es $nombre");
+  num sueldoPasadp = 7895.1;
+  num nuevoSueldo = 4588.7;
+  print("el sueldo es ${sueldoPasadp+nuevoSueldo}");
+}
---
+++ variablesConstantes.dart
@@ -0,0 +1,27 @@
+
+
+/* String int double num var
+var almacena valores sin especificar el tipo
+num para mnumeros
+*/
+
+import 'dart:io';
+
+var texto = "hola gil";
+var numero = 78;
+String palabra = "hola amiguito";
+int entero = 18;
+double decimal = 18.2;
+bool estadoDeVerdad = true;
+num numerito = 28;
+
+// palabra reservada final para declarar una constante
+final String saludo = "Bienvenido cabeza de pinga";
+void main(){
+    print(texto);
+    print(texto+palabra);
+    print(estadoDeVerdad);
+    print(entero+decimal);
+    print(texto+entero.toString());
+print(saludo);
+}
